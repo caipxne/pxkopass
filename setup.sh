92afd80c1a4789cf503bb8e8a21c59660a0275d1
caipxne <151857246+caipxne@users.noreply.github.com>
2023-12-13 12:17:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,8 +89,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=26581
-LAST_PORT=26781
+FIRST_PORT=34432
+LAST_PORT=34631
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
